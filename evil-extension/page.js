7a654f24cb6ba7e73f8b4dd86f6e72b9d3889b69
tyc105u <45002199+tyc105u@users.noreply.github.com>
2019-04-29 22:02:28 +0800

    Add files via upload

--- evil-extension/page.js
+++ evil-extension/page.js
@@ -12,9 +12,35 @@ const MATCH_LIST = {
 
 function transformTextNodes(node) {
   // TODO(you): Implement this function! See HW spec for details.
+  if (node.nodeType === Node.TEXT_NODE) {
+    var s = [];
+    var s = node.textContent.split(" ");
+    for (var i = 0; i < s.length; i++) {
+      s[i] = s[i].trim();
+      if (s[i] == "" || s[i] === undefined) {
+        s.splice(i, 1);
+        i--;
+      }
+    }
+
+    for (var i = 0; i < s.length; i++) {
+      var s2 = "";
+      if (s[i] in MATCH_LIST)
+        s2 += MATCH_LIST[s[i]] + " ";
+      else
+        s2 += s[i] + " ";
+    }
+
+    node.textContent = s2;
+  }
+
+  for(const child of node.childNodes)
+		transformTextNodes(child);
+	
 }
 
 transformTextNodes(document.body);
 
 // Log statement to test that the extension loaded properly.
 console.log('Evil extension loaded!');
+console.log('Extension updated');
